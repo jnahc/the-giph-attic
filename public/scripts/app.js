4a11ce3a80082599198902299be75ba246affe3b
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2019-10-21 12:26:55 -0700

    more semantic labeling and started setting up selectors

---
+++ public/scripts/app.js
@@ -0,0 +1,26 @@
+console.log(`yup`);
+
+// -------------------- SELECTORS
+
+// HOMEPAGE
+$signup = $(`#signup`);
+$login = $(`#login`);
+
+// SIGN UP PAGE
+$fullname = $(`#fullname`);
+$email = $(`#email`)
+$password = $(`#password`);
+$password2 = $(`#password2`);
+$favoritetopic = $(`#favoritetopic`);
+$favoritetopic2 = $(`#favoritetopic2`);
+$agecheck = $(`#agecheck`);
+$submit = $(`#submit`);
+
+// LOGIN
+
+// already existing --- email, password, submit
+
+// SHOW PAGE
+$logout = $(`#logout`);
+
+// EVENT LISTENERS
