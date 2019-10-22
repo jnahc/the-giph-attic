29bf628d06cdb996f17a52e2fcecfc224945f6e3
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2019-10-22 13:42:23 -0700

    fixed file paths and updated onSuccess

--- public/scripts/profile-jeff.js
+++ public/scripts/profile-jeff.js
@@ -2,18 +2,20 @@ console.log(`profile-jeff connected`)
 
 const userId = window.location.pathname.split(`/`)[2];
 
-const onSuccess = (response)=>{
+const onWorking = (response)=>{
   console.log(response);
   $(`#welcome-user`).empty();
   $(`#welcome-user`).append(`Welcome, ${response.data.name}`);
   $(`#topic-1-header`).empty();
-  $(`#topic-2-header`).append(``)
+  $(`#topic-1-header`).append(`${response.data.topic}`)
+  $(`#topic-2-header`).empty();
+  $(`#topic-2-header`).append(`${response.data.topic2}`)
 }
 
 $.ajax({
   method: `GET`,
   url: `http://localhost:3000/api/v1/profile/${userId}`,
-  success: onSuccess,
+  success: onWorking,
   error: (error) => {
     console.log({error})
   }
